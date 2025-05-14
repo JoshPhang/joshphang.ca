4e538e6d2f12302dad6839ccbb67555ef7b84edb
Josh Phang <jphang@sfu.ca>
2025-05-14 11:10:44 -0700

    Updated resume

--- src/app/Pages/education.tsx
+++ src/app/Pages/education.tsx
@@ -1,52 +1,56 @@
-import "../Styles/education.css"
-import Link from 'next/link'
+import "../Styles/education.css";
+import Link from "next/link";
 
 export default function Education() {
-    return (
-        <div className="education-page" id="education">
-            <div className="educations-title">Education</div>
-            <div className="quarter-border-educ"></div>
+  return (
+    <div className="education-page" id="education">
+      <div className="educations-title">Education</div>
+      <div className="quarter-border-educ"></div>
 
-            <div className="education-timeline">
-                {/* First education: UNBC */}
-                <div className="education-date e1-anim">09/2021 - 05/2025</div>
-                <div className="education-center-thin e1-anim">
-                    <div className="education-dot"></div>
-                </div>
-                <div className="education-title e1-anim">
-                    University of Northern British Columbia
-                </div>
-
-                <div className="education-role"></div>
-                <div className="education-center-thick educ1 e1-anim">
-                    <div className="education-line educ1"></div>
-                </div>
-                <div className="education-desc educ1 e1-anim">
-                    <ul>
-                        <li>BSc Computer Science</li>
-                        <li>Mathematics Minor</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className="resume">
-                Need more? Check out my resume here!
-                <Link className="resume-download-btn" href="/assets/Josh Phang's Resume.pdf" download="Josh Phang's Resume.pdf" target="_blank" rel="noopener noreferrer">
-                    <img src="/assets/download.png" className="download-icon" />
-                    <div className="download-txt">
-                        Josh Phang&apos;s Resume
-                    </div>
-                </Link>
-            </div>
-
-            <div className="scroll-to-top">
-                <Link href="/#home" className="scroll-btn">
-                <div className="scroll-box">
-                    <img src="/assets/arrow.png" className="scroll-icon" />
-                    <div className="scroll-txt">Scroll to Top</div>
-                </div>
-                </Link>
-            </div>
+      <div className="education-timeline">
+        {/* First education: UNBC */}
+        <div className="education-date e1-anim">09/2021 - 05/2025</div>
+        <div className="education-center-thin e1-anim">
+          <div className="education-dot"></div>
         </div>
-    )
+        <div className="education-title e1-anim">
+          University of Northern British Columbia
+        </div>
+
+        <div className="education-role"></div>
+        <div className="education-center-thick educ1 e1-anim">
+          <div className="education-line educ1"></div>
+        </div>
+        <div className="education-desc educ1 e1-anim">
+          <ul>
+            <li>BSc Computer Science</li>
+            <li>Mathematics Minor</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="resume">
+        Need more? Check out my resume here!
+        <Link
+          className="resume-download-btn"
+          href="/assets/Joshua_Phang_Resume.pdf"
+          download="Joshua_Phang_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/assets/download.png" className="download-icon" />
+          <div className="download-txt">Josh Phang&apos;s Resume</div>
+        </Link>
+      </div>
+
+      <div className="scroll-to-top">
+        <Link href="/#home" className="scroll-btn">
+          <div className="scroll-box">
+            <img src="/assets/arrow.png" className="scroll-icon" />
+            <div className="scroll-txt">Scroll to Top</div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/app/Pages/navbar.tsx
+++ src/app/Pages/navbar.tsx
@@ -1,25 +1,42 @@
-import "../Styles/navbar.css"
-import Link from 'next/link'
+import "../Styles/navbar.css";
+import Link from "next/link";
 
 export default function Navbar() {
-    return (
-      <div className="navbar">
-        <meta name="viewport" content="width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=no" />
-        
-        <Link href="/#home" className="logo navbar-logo"><img src="../favicon.ico" className="clickable"/></Link>
-        <Link className="navbar-text clickable" href="/#home">Home</Link>
-        <Link className="navbar-text clickable" href="/#about">About</Link>
-        <Link className="navbar-text clickable" href="/#projects">Projects</Link>
-        <Link className="navbar-text clickable" href="/#education">Education</Link>
+  return (
+    <div className="navbar">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=no"
+      />
 
-        <div className="navbar-item right">
-          <Link className="navbar-resume-download-btn" href="/assets/Josh Phang's Resume.pdf" download="Josh Phang's Resume.pdf" target="_blank" rel="noopener noreferrer">
-              <img src="/assets/download-drk.png" className="download-icon" />
-              <div className="download-txt">
-                  Josh Phang&apos;s Resume
-              </div>
-            </Link>
-        </div>
+      <Link href="/#home" className="logo navbar-logo">
+        <img src="../favicon.ico" className="clickable" />
+      </Link>
+      <Link className="navbar-text clickable" href="/#home">
+        Home
+      </Link>
+      <Link className="navbar-text clickable" href="/#about">
+        About
+      </Link>
+      <Link className="navbar-text clickable" href="/#projects">
+        Projects
+      </Link>
+      <Link className="navbar-text clickable" href="/#education">
+        Education
+      </Link>
+
+      <div className="navbar-item right">
+        <Link
+          className="navbar-resume-download-btn"
+          href="/assets/Joshua_Phang_Resume.pdf"
+          download="Joshua_Phang_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/assets/download-drk.png" className="download-icon" />
+          <div className="download-txt">Josh Phang&apos;s Resume</div>
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
+}
